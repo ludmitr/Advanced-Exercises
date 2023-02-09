7213ac789222588d1e135f65f1ada8fb856ef9e1
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-02-09 18:40:38 +0200

    renamed inside parameters for more clearence

--- ex2.py
+++ ex2.py
@@ -2,7 +2,7 @@ import math
 
 def lathice_path(x:int,y:int):
     """
-    recursive function that will return number of paths
+    function that will return number of paths
 
     Parameters:
     x(int): represent x position of a point
@@ -12,17 +12,18 @@ def lathice_path(x:int,y:int):
     count(int): number of path to reach (x,y) point avaible
     
     """
+    binom_n = x+y
+    binom_k = 2
 
-
-    if y == 1 or y == x:
+    if binom_k == 1 or y == binom_n:
         return(1)
 
-    if y > x:
+    if binom_k > binom_n:
         return(0)        
     else:
-        a = math.factorial(x)
-        b = math.factorial(y)
-        div = a // (b*(x-y))
+        a = math.factorial(binom_n)
+        b = math.factorial(binom_k)
+        div = a // (b*(binom_n-binom_k))
         return(div) 
 
-print(lathice_path(2,2))
+print(lathice_path(5,5))
