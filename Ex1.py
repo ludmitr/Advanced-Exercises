04fc7ce3d3c30017bc7013669885e18fa763cc78
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-02-09 06:06:06 +0200

    improved code to calculate faster

--- Ex1.py
+++ Ex1.py
@@ -13,22 +13,16 @@ def list_prime (prime_index:int) -> int:
     >>> last_prime(5)
     11
     """
-    prime_list = [1,2]
-    highest_prime = 2
-    if prime_index == 1:
-        return prime_list[0]
-    elif prime_index == 2:
-        return prime_list[1]
-    else:
-        while len(prime_list) == prime_index:
-            highest_prime +=1
-            is_prime = True
-            for x in range(2,int((highest_prime+1)/2) ):
-                if (highest_prime % x) == 0:
-                    is_prime = False
-            if is_prime:
-                prime_list.append(highest_prime)
-                # if highest_prime % x != 0
+    prime_list = []
+    current_number = 1
+    while len(prime_list) != prime_index:
+        if prime_check(current_number):
+            prime_list.append(current_number)
+
+        current_number += 1
+    
+    return prime_list[prime_index - 1]  
+
                 
 def prime_check(num:int)-> bool:
     """
@@ -41,13 +35,20 @@ def prime_check(num:int)-> bool:
     True: if its a prime number
     False: if its not a prime number
     """
-    is_prime = True
-    
-    for x in range(2,int((num+2)/2) ):
-                if (num % x) == 0:
-                    is_prime = False
+    is_prime = False
+    if num == 1:
+        return False
+    if num == 2:
+        return True
+    if num == 3:
+        return True
+    if num % 6 in [1,5]:  
+        is_prime = True
+        for x in range(2,int((num+2)/2) ):
+                    if (num % x) == 0:
+                        is_prime = False
 
     return is_prime
 
 
-print(prime_check())
+print(list_prime(10001))
