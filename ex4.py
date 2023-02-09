c74701d2537e38fe6054e5574e16f092cd96cc5f
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-02-10 01:47:42 +0200

    adding docstrings to ex4

--- ex4.py
+++ ex4.py
@@ -1,6 +1,9 @@
 def factorial(num:int)-> int:
     """
+    Calculate factorial of num
     
+    Returns:
+    num(int): calculated factorial
     """
     if  num ==1:
         return num*1
@@ -9,6 +12,17 @@ def factorial(num:int)-> int:
 
     return num
 def count_number(num:int)->int:
+    """
+    Calculating the sum of each character in num
+
+    Returns:
+    sum(int)  calculated number of a num
+
+    
+    Example:
+    >>> count_number(22)
+    4
+    """
     number_list = [int(number) for number in str(num)]
     sum = 0
     for x in number_list:
