53159dee484320beef2fd9682b50e71900b2c7ba
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-02-09 17:28:31 +0200

    writing function it self

--- ex2.py
+++ ex2.py
@@ -5,9 +5,19 @@ def lathice_path(x:int,y:int):
     Parameters:
     x(int): represent x position of a point
     y(int): represent y position of a point
-    
+
     Returns
     count(int): number of path to reach (x,y) point avaible
     
     """
-    pass
+    if x == 0 and y == 0:
+        return 1
+
+    count = 0
+
+    if x > 0:
+        count += lathice_path(x-1,y)
+    if y > 0:
+        count += lathice_path(x,y-1) 
+    
+    return count
