f73b58050ca7f01bea221526cec74178c4373252
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-02-09 17:23:45 +0200

    defining a function and adding docsrting

---
+++ ex2.py
@@ -0,0 +1,13 @@
+def lathice_path(x:int,y:int):
+    """
+    recursive function that will return number of paths
+
+    Parameters:
+    x(int): represent x position of a point
+    y(int): represent y position of a point
+    
+    Returns
+    count(int): number of path to reach (x,y) point avaible
+    
+    """
+    pass
