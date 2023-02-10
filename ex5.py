4b4277409b7117cc9d6ea1f6b14a8bcd78ce0e4a
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-02-10 03:04:13 +0200

    some comments in ex5

--- ex5.py
+++ ex5.py
@@ -7,10 +7,12 @@ def is_trunctable(num):
     is_left = True
     is_right = True
 
+    # each loop run remove number from left and check if its prime
     for x in range(1,len(s_number)):
         if sympy.isprime(int(s_number[x:])) == False:
             is_left = False
             break
+    # each loop run remove number from left and check if its prime
     for x in range(len(s_number)-1,0,-1):
         if sympy.isprime(int(s_number[:x])) == False:
             is_right = False
