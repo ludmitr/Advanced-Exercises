a5f90e961c94c1870cf567b4c29d5a50a39428c3
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-03-02 04:35:09 +0200

    adding new function decrypt text

--- break_substitution.py
+++ break_substitution.py
@@ -1,6 +1,6 @@
 import string
 
-book = "encrypted_book.txt"
+book_file_name = "encrypted_book.txt"
 
 
 def get_book_content(file_path):
@@ -12,7 +12,7 @@ def sort_dictionary_by_value(dictionary: dict):
     """
     Creates dictionary with descending order by its value
 
-    Returns:(dict) dictionary with descending order by its value
+    Returns: dictionary with descending order by its value
     """
     # creating copy of dictionary for local logic
     temp_dictionary = dictionary.copy()
@@ -27,6 +27,7 @@ def sort_dictionary_by_value(dictionary: dict):
         descending_dictionary[highest_key] = highest_value
         del temp_dictionary[highest_key]
     return descending_dictionary
+
 
 def frequency_analysis(text: str):
     """
@@ -45,14 +46,39 @@ def frequency_analysis(text: str):
             if char not in chars_frequency_dictionary:
                 chars_frequency_dictionary[char] = 0
             chars_frequency_dictionary[char] += 1
+
     return chars_frequency_dictionary
 
+def decrypt_text(text,dictionary):
+    new_dictionary = {
+        "i":"e", "k":"t", "x":"a", "u":"o", "t":"i",
+        "w":"n", "a":"s", "r":"r", "f":"h", "c":"d",
+        "n":"l", "v":"m", "h":"c", "j":"y", "z":"g",
+        "d":"f", "o":"u", "m":"w", "q":"p",  "e":"k",
+        "l":"v", "s":"j", "b":"b",  "p":"q", "g":"x",
+        "y":"z"
+    }
 
+
+    new_text = ""
+    for char in text:
+        if char.lower() in new_dictionary:
+            if char.islower():
+                new_text += new_dictionary[char]
+            else:
+                new_text += new_dictionary[char].upper()
+
+
+    return new_text
 def main():
-    encrypted_book = get_book_content(book)
+    encrypted_book = get_book_content(book_file_name)
     temp_dictionary = frequency_analysis(encrypted_book)
-    for x, y in temp_dictionary.items():
-        print(f"{x}:  {y}")
+    temp_dictionary = sort_dictionary_by_value(temp_dictionary)
+    temp_dictionary = decrypt_text(encrypted_book,temp_dictionary)
+
+    print(temp_dictionary)
+    # for x, y in temp_dictionary.items():
+    #     print(f"{x}:  {y}")
 
 
 if __name__ == "__main__":
