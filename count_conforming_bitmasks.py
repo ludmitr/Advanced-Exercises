6fa19cc0a6aae7a166484296da330130ee22e3c1
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-03-02 12:41:55 +0200

    created new function create_binary_variations and finished conforming_numbers. both working

--- count_conforming_bitmasks.py
+++ count_conforming_bitmasks.py
@@ -13,11 +13,39 @@ def solution(a: int, b: int, c: int):
     conforming_list_c = conforming_numbers(bin(c)[2:])
 
 def conforming_numbers(b_number: str) -> list:
-    dict_number = {}
+    """
+    function counts all possible binary numbers that conforming b_number
+    Args:
+        b_number: (str)
+
+    Returns: list of all possible binary numbers that conforming b_number. each element in list is a string
+
+    """
+    zero_indexes_list = []
+    conforming_list = []
+    # creating list where each element is original index of 0 in b_number
     for x in range(len(b_number)):
-        if b_number[x] == "0": dict_number[x] = int(b_number[x])
+        if b_number[x] == "0": zero_indexes_list.append(x)
 
+    variations_list = create_binary_variations(len(zero_indexes_list))
+    # creating list of binary numbers that conform b_number
+    for list in variations_list:
+        counter = 0
+        temp_number = b_number
+        for index in zero_indexes_list:
+            temp_number = temp_number[:index] + str(list[counter]) + temp_number[index+1:]
+            counter += 1
+        conforming_list.append(temp_number)
+    return conforming_list
 
+def create_binary_variations(num):
+    main_list = []
+    for i in range(1,2 ** num):
+        binary_string = format(i, '0{}b'.format(num))
+        new_list = [int(bit) for bit in binary_string]
+        main_list.append(new_list)
+
+    return main_list
 
     return b_number,dict_number
 
@@ -28,5 +56,7 @@ def main():
     a = conforming_numbers(bin(455)[2:])
     for x in a:
         print(x)
+
+
 if __name__ == "__main__":
     main()
